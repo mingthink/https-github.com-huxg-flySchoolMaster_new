5bef0640c3ede8183d4e0b880e78cf607e81ca6c
caiyc <1376648068@qq.com>
2014-09-09 13:47:11 +0800

    增加一个文件

---
+++ FlySchoolMaster/MTContantPersonModel.h
@@ -0,0 +1,16 @@
+//
+//  MTContantPersonModel.h
+//  FlySchoolMaster
+//
+//  Created by caiyc on 14-9-9.
+//  Copyright (c) 2014年 MingThink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTContantPersonModel : NSObject
+@property(nonatomic,retain)NSString *name;
+@property(nonatomic,retain)NSString *phonenum;
+@property(nonatomic,retain)NSString *address;
+@property(nonatomic,retain)NSString *authcodes;
+@end
---
+++ FlySchoolMaster/MTContantPersonModel.m
@@ -0,0 +1,13 @@
+//
+//  MTContantPersonModel.m
+//  FlySchoolMaster
+//
+//  Created by caiyc on 14-9-9.
+//  Copyright (c) 2014年 MingThink. All rights reserved.
+//
+
+#import "MTContantPersonModel.h"
+
+@implementation MTContantPersonModel
+
+@end
