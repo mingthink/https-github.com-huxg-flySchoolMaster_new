ba497d909b65d392d5d54da4793c58b99ae0b696
huxg <mingthink_ios@126.com>
2014-09-12 15:20:52 +0800

    sas

--- FlySchoolMaster/MTDetailViewController.m
+++ FlySchoolMaster/MTDetailViewController.m
@@ -18,7 +18,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization萨萨说sssss
+        // Custom initialization萨萨说sssss说
     }
     return self;
 }
